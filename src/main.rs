aa5ca0087f421f482332af0eae9bc8c9d1592f66
DjebbZ <khalid.jebbari@gmail.com>
2020-08-06 00:29:38 +0200

    It works \o/

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,7 @@ use std::fs::read_to_string;
 use std::io;
 use std::io::{Error, ErrorKind};
 use std::num::ParseIntError;
-use std::process::Command;
+use std::process::{Command, ExitStatus};
 use std::thread::sleep;
 use std::time::Duration;
 
@@ -46,6 +46,7 @@ impl From<ParseIntError> for ReadError {
 }
 
 /// Read the file and return its content, which is supposed to be a single value in a single line.
+/// TODO: use OSStr/OSPath types from std instead
 fn read_value(path: &str) -> Result<f64, ReadError> {
     let raw = read_to_string(path)?;
 
@@ -60,127 +61,156 @@ fn read_value(path: &str) -> Result<f64, ReadError> {
 }
 
 /// Helper function to reduce duplication of code when invoking an external command
-fn invoke_rotation(command: &mut Command, err_msg: &str) -> io::Result<()> {
-    match command.status() {
-        Ok(status) => {
-            if status.success() {
-                Ok(())
-            } else {
-                Err(io::Error::new(ErrorKind::Other, err_msg))
-            }
-        }
-        Err(err) => Err(err),
+fn invoke_rotation(orientation: &str, err_msg: &str) -> io::Result<()> {
+    let status = Command::new("xrandr")
+        .args(&["--orientation", orientation])
+        .status()
+        .expect("Couldn't run xrandr, is it properly installed?");
+
+    if status.success() {
+        Ok(())
+    } else {
+        Err(io::Error::new(ErrorKind::Other, err_msg))
     }
 }
 
 /// Using xrandr, rotate the current output based on the laptop orientation.
-fn rotate(orientation: &LaptopOrientation) -> io::Result<()> {
-    // instantiate once to avoid duplication
-    let mut xrandr = Command::new("xrandr");
-    let inputs = list_inputs()?;
-
-    let screen_rotated = match orientation {
+fn rotate_screen_output(orientation: &LaptopOrientation) -> io::Result<()> {
+    match orientation {
         LaptopOrientation::Normal | LaptopOrientation::Tablet => invoke_rotation(
-            xrandr.args(&["--orientation", "normal"]),
+            "normal",
             "xrandr couldn't rotate screen in normal orientation",
-        ),
-
-        LaptopOrientation::PortraitLeft => invoke_rotation(
-            xrandr.args(&["--orientation", "right"]),
-            "xrandr couldn't rotate screen right",
-        ),
-        LaptopOrientation::PortraitRight => invoke_rotation(
-            xrandr.args(&["--orientation", "left"]),
-            "xrandr couldn't rotate screen to the left",
-        ),
-        LaptopOrientation::Tent => invoke_rotation(
-            xrandr.args(&["--orientation", "inverted"]),
-            "xrandr couldn't rotate screen 180\u{b0}",
-        ),
+        )?,
+        LaptopOrientation::PortraitLeft => {
+            invoke_rotation("right", "xrandr couldn't rotate screen right")?
+        }
+        LaptopOrientation::PortraitRight => {
+            invoke_rotation("left", "xrandr couldn't rotate screen to the left")?
+        }
+        LaptopOrientation::Tent => {
+            invoke_rotation("inverted", "xrandr couldn't rotate screen 180\u{b0}")?
+        }
     };
 
-    screen_rotated.and_then(|_| adjust_keyboard(orientation, &inputs))
+    Ok(())
 }
 
 /// Returns elements in `inputs` that match the elements in `to_find`.
-/// Elements of `to_find` must be substrings contained in elements of `inputs`
+/// Elements in `to_find` must be substrings of elements in `inputs`
 fn find_inputs<'a>(inputs: &'a [String], to_find: &'a [String]) -> Vec<&'a String> {
     inputs
         .iter()
         .filter(|device| {
             to_find.iter().any(|tofind| {
-                tofind
+                device
                     .to_ascii_lowercase()
-                    .contains(&device.to_ascii_lowercase())
+                    .contains(&tofind.to_ascii_lowercase())
             })
         })
         .collect::<Vec<&String>>()
 }
 
-fn adjust_keyboard(orientation: &LaptopOrientation, inputs: &[String]) -> io::Result<()> {
-    let keyboard_to_find = &[String::from("AT Translated Set 2 keyboard")];
-    let keyboard: Vec<&String> = find_inputs(inputs, keyboard_to_find);
-
-    if keyboard.is_empty() {
-        return Err(io::Error::new(io::ErrorKind::Other, "No keyboard found"));
-    }
-
-    match orientation {
-        LaptopOrientation::Normal => {
-            let status = Command::new("xinput")
-                .arg("enable")
-                .arg(keyboard[0]) // `keyboard[0]` because I suppose there should be only one integrated keyboard in a laptop
-                .status()
-                .expect("Couldn't run `xinput`, are you sure it's installed properly?");
-            if !status.success() {
-                return Err(io::Error::new(
-                    ErrorKind::Other,
-                    "xinput failed to enable the keyboard",
-                ));
-            }
-        }
-        LaptopOrientation::PortraitLeft
-        | LaptopOrientation::PortraitRight
-        | LaptopOrientation::Tent
-        | LaptopOrientation::Tablet => {
-            let status = Command::new("xinput")
-                .arg("disable")
-                .arg(keyboard[0])
-                .status()
-                .expect("Couldn't run `xinput`, are you sure it's installed properly?");
-            if !status.success() {
-                return Err(io::Error::new(
-                    ErrorKind::Other,
-                    "xinput failed to disable the keyboard",
-                ));
-            }
+/// Using `xinput`, enable or disable the input devices
+fn toggle_inputs(inputs: &[&String], enable: bool) -> io::Result<()> {
+    for input in inputs {
+        let action = if enable { "enable" } else { "disable" };
+        let failure_msg = format!("xinput couldn't {} {}", action, input);
+        let status = Command::new("xinput")
+            .arg(action)
+            .arg(input) // `keyboard[0]` because I suppose there should be only one integrated keyboard in a laptop
+            .status()
+            .expect("Couldn't run `xinput`, are you sure it's installed properly?");
+        if !status.success() {
+            return Err(io::Error::new(ErrorKind::Other, failure_msg));
         }
     }
 
     Ok(())
 }
 
-// fn adjust_touchscreens(orientation: &LaptopOrientation, inputs: Vec<&str>) -> io::Result<()> {
-//     let touchscreens: Vec<&str> = inputs
-//         .iter()
-//         .filter(|&device| {
-//             device.to_ascii_lowercase().contains("touchscreen")
-//                 || device.to_ascii_lowercase().contains("wacom")
-//         })
-//         .map(|dev| *dev)
-//         .collect();
-// }
-//
-// fn adjust_touchpads(orientation: &LaptopOrientation, inputs: Vec<&str>) -> io::Result<()> {
-//     let touchpads: Vec<&str> = inputs
-//         .iter()
-//         .filter(|device| {
-//             device.to_ascii_lowercase().contains("touchpad")
-//                 || device.to_ascii_lowercase().contains("trackpoint")
-//         })
-//         .map(|dev| *dev)
-//         .collect();
-// }
+fn adjust_keyboard(orientation: &LaptopOrientation, inputs: &[String]) -> io::Result<()> {
+    // Singular tense because there should be only one internal keyboard in a laptop, right?
+    let keyboard_to_find = &[String::from("AT Translated Set 2 keyboard")];
+    let keyboard: Vec<&String> = find_inputs(inputs, keyboard_to_find);
+
+    if keyboard.is_empty() {
+        return Err(io::Error::new(io::ErrorKind::NotFound, "No keyboard found"));
+    }
+
+    match orientation {
+        LaptopOrientation::Normal => toggle_inputs(&keyboard, true)?,
+        LaptopOrientation::PortraitLeft
+        | LaptopOrientation::PortraitRight
+        | LaptopOrientation::Tent
+        | LaptopOrientation::Tablet => toggle_inputs(&keyboard, false)?,
+    }
+
+    Ok(())
+}
+
+fn rotate_screen_inputs(orientation: &LaptopOrientation, inputs: &[String]) -> io::Result<()> {
+    let screen_inputs_to_find = &[String::from("touchscreen"), String::from("wacom")];
+    let screen_inputs = dbg!(find_inputs(inputs, screen_inputs_to_find));
+
+    if screen_inputs.is_empty() {
+        return Err(io::Error::new(
+            ErrorKind::NotFound,
+            "no touchscreen or wacom inputs found",
+        ));
+    }
+
+    let transformation_matrix = match orientation {
+        LaptopOrientation::Normal | LaptopOrientation::Tablet => [1, 0, 0, 0, 1, 0, 0, 0, 1],
+        LaptopOrientation::PortraitLeft => [0, 1, 0, -1, 0, 1, 0, 0, 1],
+        LaptopOrientation::PortraitRight => [0, -1, 1, 1, 0, 0, 0, 0, 1],
+        LaptopOrientation::Tent => [-1, 0, 1, 0, -1, 1, 0, 0, 1],
+    };
+
+    for input in screen_inputs {
+        let mut xinput = Command::new("xinput");
+        let command = xinput
+            .arg("set-prop")
+            .arg(input)
+            .arg("Coordinate Transformation Matrix");
+
+        for number in &transformation_matrix {
+            command.arg(number.to_string());
+        }
+        let status = command
+            .status()
+            .expect("Couldn't run `xinput`, are you sure it's installed properly?");
+        if !status.success() {
+            return Err(io::Error::new(
+                ErrorKind::Other,
+                format!("xinput couldn't rotate '{}'", input),
+            ));
+        }
+    }
+
+    Ok(())
+}
+
+fn adjust_touchpads(orientation: &LaptopOrientation, inputs: &[String]) -> io::Result<()> {
+    let touchpad_to_find = &[String::from("Touchpad"), String::from("Trackpoint")];
+    let touchpads = find_inputs(inputs, touchpad_to_find);
+
+    if touchpads.is_empty() {
+        return Err(io::Error::new(
+            ErrorKind::NotFound,
+            "no touchpad or trackpoint found",
+        ));
+    }
+
+    match orientation {
+        LaptopOrientation::Normal => toggle_inputs(&touchpads, true)?,
+        LaptopOrientation::PortraitLeft
+        | LaptopOrientation::PortraitRight
+        | LaptopOrientation::Tent
+        | LaptopOrientation::Tablet => toggle_inputs(&touchpads, false)?,
+    }
+
+    Ok(())
+}
 
 /// Using xinput, list the available inputs.
 fn list_inputs() -> io::Result<Vec<String>> {
@@ -281,9 +311,9 @@ struct Accelerometer {
 impl Accelerometer {
     pub fn new(x: f64, y: f64, z: f64, scale: f64, offset: f64) -> Accelerometer {
         Accelerometer {
-            x: dbg!(normalize(x, scale, offset)),
-            y: dbg!(normalize(y, scale, offset)),
-            z: dbg!(normalize(z, scale, offset)),
+            x: normalize(x, scale, offset),
+            y: normalize(y, scale, offset),
+            z: normalize(z, scale, offset),
         }
     }
 
@@ -320,8 +350,14 @@ fn main() {
 
         let current_orientation =
             Accelerometer::new(accel_x, accel_y, accel_z, scale, offset).which_orientation();
-        rotate(&current_orientation).unwrap();
+        let inputs = list_inputs().unwrap();
 
-        sleep(Duration::from_secs(5));
+        rotate_screen_output(&current_orientation)
+            .and_then(|_| adjust_keyboard(&current_orientation, &inputs))
+            .and_then(|_| adjust_touchpads(&current_orientation, &inputs))
+            .and_then(|_| rotate_screen_inputs(&current_orientation, &inputs))
+            .unwrap();
+
+        sleep(Duration::from_secs(3));
     }
 }
